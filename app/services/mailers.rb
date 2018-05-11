9e0fc183b2410466afcdf3591782847a359aa45a
friedausha <hallofrieda@gmail.com>
2018-05-11 21:02:18 +0700

    add sendgrid

--- app/services/mailers.rb
+++ app/services/mailers.rb
@@ -1,16 +1,16 @@
-# require 'sendgrid-ruby'
-# include SendGrid
-# class Mailers
-#   def new_reservation_email(laboratory)
-#   from = Email.new(email: 'dummy.frieda@gmail.com')
-#   subject = 'New Reservation'
-#   to = Email.new(email: 'dummy.frieda@gmail.com')
-#   content = Content.new(type: 'text/plain', value: 'Hi, #{laboratory}! Ada reservasi baru tuh!')
-#   mail = Mail.new(from, subject, to, content)
-#   # puts JSON.pretty_generate(mail.to_json)
-#   puts mail.to_json
-#
-#   sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'], host: 'https://api.sendgrid.com')
-#   response = sg.client.mail._('send').post(request_body: mail.to_json)
-#   end
-# end
+require 'sendgrid-ruby'
+include SendGrid
+class Mailers
+  def new_reservation_email(laboratory)
+  from = Email.new(email: 'dummy.frieda@gmail.com')
+  subject = 'New Reservation'
+  to = Email.new(email: 'dummy.frieda@gmail.com')
+  content = Content.new(type: 'text/plain', value: 'Hi, #{laboratory}! Ada reservasi baru tuh!')
+  mail = Mail.new(from, subject, to, content)
+  # puts JSON.pretty_generate(mail.to_json)
+  puts mail.to_json
+
+  sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'], host: 'https://api.sendgrid.com')
+  response = sg.client.mail._('send').post(request_body: mail.to_json)
+  end
+end
