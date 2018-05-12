bb7e7f44d9a7076ecb7c31d58fd309101d56c4e8
friedausha <hallofrieda@gmail.com>
2018-05-12 14:31:46 +0700

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => "home#index"
-  resources :reservation, only: [:create,:update]
-  resources :computer, only: [:create, :show, :update, :destroy]
+  resources :reservation
+  resources :computer
   resources :laboratory
-  
+
   match '*path', to: 'application#route_not_found',
                  via: :all,
                  as: 'route_not_found'
