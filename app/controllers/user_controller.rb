a0658c3333e2048776a61ac8562f5bfacda8c121
friedausha <hallofrieda@gmail.com>
2018-05-19 13:12:52 +0700

    update user

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -13,6 +13,18 @@ class UserController < ApplicationController
   end
 
   def update
+    permitted = Authenticator.new(authorization:
+      request.headers['Authorization']).user_permitted?
+    return render json: { status: 403 } unless permitted
 
+    user = User.where(id: params['id']).first
+    return render json: { status: 404 } unless inventory
+
+    user.update_attributes(name: params['name']) if params['name']
+    user.update_attributes(nrp: params['nrp']) if params['nrp']
+    user.update_attributes(email: params['email']) if params['email']
+    user.update_attributes(password: params['password']) if params['password']
+    user.update_attributes(phone_number: params['phone_number']) if params['phone_number']
+    render json: { status: 200 }
   end
 end
